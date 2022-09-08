0df17df9d9084fef6df8d392fc5f9919141551a1
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-08 21:26:22 +0530

    Adding Program for creating Product review class with ProductID, UserID, Rating, Review and isLike fields

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ProductReviewManagementUsingLinq;
+
 Console.WriteLine("Wellcome to the Product review management Problem using linq");
+List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProducID=1,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProducID=3,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProducID=4,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProducID=5,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProducID=6,UserID=1,Rating=1,Review="bas",isLike=true},
+                new ProductReview(){ProducID=8,UserID=1,Rating=10,Review="Good",isLike=false},
+                new ProductReview(){ProducID=8,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProducID=10,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProducID=11,UserID=1,Rating=3,Review="nice",isLike=true}
+
+            };
+
+foreach (var list in productReviewList)
+{
+    Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+        + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+}
