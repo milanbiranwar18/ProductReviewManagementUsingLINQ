c91a5eb7d629b90a3117d026d4083f0e434dbf3b
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-08 21:33:59 +0530

    Program for Retrieve top 3 records from the list who’s rating is high using LINQ

---
+++ Management.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementUsingLinq
+{
+    public class Management
+    {
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+
+
+        }
+    }
+}
